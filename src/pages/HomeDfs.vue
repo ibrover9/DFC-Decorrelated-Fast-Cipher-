291d582a30210c0f28d9afeac7c95b098abeb76a
Artur Novikov <artur.20177102@mail.ru>
2025-01-21 20:29:18 +0300

    delete functions in HomeDFC

--- src/pages/HomeDfs.vue
+++ src/pages/HomeDfs.vue
@@ -181,56 +181,6 @@ function splitMessage(X) {
   return { left, right }
 }
 
-// Функция для разделения ключа на две части по 64 бита
-function splitKey(Key) {
-  console.log(Key)
-  const left = Key >> 64n // Левая половина
-  const right = Key & 0xffffffffffffffffn // Правая половина
-  console.log(`Split Key: Ai=${left.toString(16)}, Bi=${right.toString(16)}`)
-  return { left, right }
-}
-
-// function roundDecryption(R, L, key, odd) {
-//   const { left: Ai, right: Bi } = splitKey(key)
-
-//   // Шаг 1: Вычисляем Z
-//   const Z = ((Ai * L + Bi) % MOD64_PLUS_13) % MOD64
-//   console.log(`Computed Z: ${Z.toString(16)}`)
-
-//   // Разделяем Z на две части по 32 бита
-//   const Zl = Z >> 32n // Старшие 32 бита
-//   const Zr = Z & 0xffffffffn // Младшие 32 бита
-//   console.log(`Split Z: Zl=${Zl.toString(16)}, Zr=${Zr.toString(16)}`)
-
-//   // Транскрипция Zl для S-блока
-//   const truncatedZl = Zl & 0x3fn // Обрезаем до 6 младших бит
-//   console.log(`Truncated Zl for RT: ${truncatedZl.toString(16)}`)
-
-//   // Применяем S-блок RT
-//   const RTResult = RT(truncatedZl)
-//   console.log(`RT Table Output for ${truncatedZl}: ${RTResult.toString(16)}`)
-
-//   // Вычисляем результат Y для расшифрования
-//   const Yl = (Zr ^ BigInt(RTResult)) << 32n // Левые 32 бита результата
-//   const Yr = ((Zl ^ KC) + (KD % MOD64)) & 0xffffffffn // Правые 32 бита результата
-//   console.log(`Yl: ${Yl.toString(16)}, Yr: ${Yr.toString(16)}`)
-
-//   // Объединяем Y
-//   const Y = Yl | Yr
-//   console.log(`Combined Y: ${Y.toString(16)}`)
-
-//   // Финальный результат (шифрование)
-//   if (odd) {
-//     const finalResult = (Y ^ R) | (L << 64n)
-//     console.log(`Final Combined Result (hex):112 ${finalResult.toString(16)}`)
-//     return finalResult
-//   } else {
-//     const finalResult = L | ((Y ^ R) << 64n)
-//     console.log(`Final Combined Result (hex): ${finalResult.toString(16)}`)
-//     return finalResult
-//   }
-// }
-
 function performEncryptionDouble(X) {
   try {
     console.log('=== Starting DFC Round (Encrypt) ===')
@@ -320,11 +270,6 @@ function performDecryptionDouble(X) {
     document.getElementById('output').textContent = `Ошибка: ${error.message}`
     console.error(error)
   }
-}
-
-// Функция RT для выполнения замены по таблице
-function RT(input) {
-  return RTTable[Number(input)]
 }
 
 function clearKeys() {
